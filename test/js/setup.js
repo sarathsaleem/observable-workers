12f39f8e5659d66012ed6959fbce43aeeadc20fb
Sarath <sarathsaleem@gmail.com>
2015-05-07 01:40:02 +0400

    Refactor code

--- test/js/setup.js
+++ test/js/setup.js
@@ -1,59 +1,85 @@
 /*
  * I have 10 tasks to do, with N numer of workers
  *
- *
- *
- *
- *
  */
-
 
 var startOne = 0,
     startFour = 0;
+var numberOfTasks = 10;
+
+
+var oneWorker = function () {
+
+    // create a worker pool with 1 worker
+    var workerPool = createWorkerPool(1);
+    startOne = Date.now();
+
+    // Assign works to workerPool
+    mapAndStartWorking(numberOfTasks, workerPool, 'oneWorker');
+};
+
+
+var nWorkerPool = function () {
+    // create a worker pool with 4 worker
+    var workerPool = createWorkerPool(4);
+    startFour = Date.now();
+
+    // Assign works to workerPool 
+    mapAndStartWorking(numberOfTasks, workerPool, 'fourWorker');
+};
+
+
+/* Create and return array of workers
+ *
+ * @param : number of workers to create
+ * @retun : array of worker
+ *
+ */
+function createWorkerPool(workerNumber) {
+    var workers = [];
+    for (var i = 0; i < workerNumber; i++) {
+        var worker = runWork();
+        workers.push(worker);
+    }
+
+    return workers;
+}
+
+
+function runWork() {
+    var worker = new Worker('js/worker.js');
+    //read response
+    worker.addEventListener('message', function (e) {
+        displayResponse(e.data);
+    }, false);
+    return worker;
+}
+
+function mapAndStartWorking(numberOfTasks, workers, name) {
+
+    for (var i = 0; i < numberOfTasks; i++) {
+
+        var mapToTask = i % workers.length;
+
+        workers[mapToTask].postMessage({
+            method: 'GET',
+            index: { // to identify who is the caller
+                i: i,
+                name: name
+            }
+        });
+    }
+}
+
+
+// UI with jquery
 
 $(function () {
 
-    var numberOfTasks = 10;
-    
-    
-    var oneWorker = function () {
-        
-        // create a worker pool with 1 worker
-        var workerPool = createWorkerPool(1);
-        startOne = Date.now();
-        
-        /* Assign works to workerPool
-         * @param : numner of takas to do
-         * @param : worker pool to run
-         * @param : identifier name
-         */
-        mapAndStartWorking(numberOfTasks, workerPool, 'oneWorker');
-    };
-
-   $('.oneWorker .start').on('click', oneWorker);
-    
-   var nWorkerPool = function () {
-        // create a worker pool with 4 worker
-        var workerPool = createWorkerPool(4);
-        startFour = Date.now();
-       
-        /* Assign works to workerPool
-         * @param : numner of takas to do
-         * @param : worker pool to run
-         * @param : identifier name
-         */
-        mapAndStartWorking(numberOfTasks, workerPool, 'fourWorker');
-    };
-
+    $('.oneWorker .start').on('click', oneWorker);
     $('.fourWorker .start').on('click', nWorkerPool);
 
 });
-
-
-
-
-
-
 
 function displayResponse(data) {
     var taskEle;
@@ -75,47 +101,10 @@ function displayResponse(data) {
         $('.oneWorker .info').addClass('done').find('span').text(Date.now() - startOne);
     }
 
-    if ($('.fourWorker .fibval.done').length === 10 &&  !$('.fourWorker .info').hasClass('done')) {
+    if ($('.fourWorker .fibval.done').length === 10 && !$('.fourWorker .info').hasClass('done')) {
         $('.fourWorker .info').addClass('done').find('span').text(Date.now() - startFour);
     }
 
-}
 
-
-function runWork() {
-    var worker = new Worker('js/worker.js');
-    //read response
-    worker.addEventListener('message', function (e) {
-        displayResponse(e.data);
-    }, false);
-    return worker;
-}
-
-
-function createWorkerPool(workerNumber) {
-    var workers = [];
-    for (var i = 0; i < workerNumber; i++) {
-        var worker = runWork();
-        workers.push(worker);
-    }
-
-    return workers;
-}
-
-
-function mapAndStartWorking(numberOfTasks, workers, name) {
-
-    for (var i = 0; i < numberOfTasks; i++) {
-
-        var mapToTask = i % workers.length;
-
-        workers[mapToTask].postMessage({
-            method: 'GET',
-            index: { // to identify who is the caller
-                i: i,
-                name: name
-            }
-        });
-    }
 
 }
